61f593a1745e5ce3281f10bfb1e8d541869e7ad0
ddsvetlov <dankasvetlov@gmail.com>
2020-06-06 23:35:11 +0300

    ref

--- task2.cc
+++ task2.cc
@@ -1,67 +1,3 @@
-// #include <iostream>
-// #include <iomanip>
-// #include <fstream>
-// #include <string>
-// #include <chrono>
-// #include <ctime>
-// #include <list>
-// #include <cstdlib>
-// #include <thread>
-
-// using namespace std;
-// using namespace std::chrono;
-// typedef list<int> LISTINT;
-// typedef list<double> LISTDOUBLE;
-
-// typedef unsigned long long ull;
-
-// inline ull rdtsc()
-// {
-//     unsigned int lo, hi;
-//     asm volatile("rdtsc\n"
-//                  : "=a"(lo), "=d"(hi));
-//     return ((ull)hi << 32) | lo;
-// }
-
-// const int static_array_size = 1024;
-// static int static_array[static_array_size];
-// const int thread_num = 1;
-// static double thread_time[thread_num];
-
-// // 
-// double serial_access_time(int size, int step)
-// {
-//     int *a = new int[size];
-//     int sum = 0;
-//     int count = 100;
-//     double time = 0;
-//     // fill array random values
-//     for (int i = 0; i < size; i++)
-//     {
-//         a[i] = rand() % (size);
-//     }
-
-//     // start timer
-//     ull time1 =rdtsc();
-//     // auto start_time = std::chrono::high_resolution_clock::now();
-
-//     // access to random element in array  
-//     for (int i = 0; i < size; i += step)
-//     {
-//         int index = rand() % (size);
-//         // do something with element in array
-//         sum += a[index];        
-//     }
-//  	// end timer
-//     // auto end_time = std::chrono::high_resolution_clock::now();
-//     ull time2 = rdtsc();
-//     time+=time2-time1;
-//     // time += std::chrono::duration_cast<std::chrono::nanoseconds>(end_time - start_time).count();
-//     cout << "size: " << size * 4 << "    avg time: " << time / size * step << endl;
-//     delete[] a;
-//     // return avg speed
-//     return size / step / time;
-// }
 
 
 // // DEPENDENT OF ACCESS TIME TO MEMORY ON THE NUMBER OF THREADS
@@ -102,64 +38,10 @@
 // }
 
 
-// int main()
-// {
-//     list<int> bytesList = {};
-//     list<double> timesList = {};
-
-//     int int_size = sizeof(int);
-//     int array_size = 1;
-//     int steps_1 = 100;
-
-//     cout << sizeof(int) << "bytes" << endl;
-
-//     // find data of bytes and times to set size of cash 
-//     for (int i = 0; i < steps_1; i++)
-//     {
-//         double time_count = serial_access_time(array_size, 1);
-//         bytesList.push_back(int_size * array_size);
-//         array_size+=10000;
-//         timesList.push_back(time_count);
-
-//     }
-
 //     // third task
 //     // call func to dependent of access time to memory on the number of threads
 //     thread_access();
 
-//     // load data to txt file to create plot
-//     ofstream x, y;
-
-//     x.open("x.txt");
-//     y.open("y.txt");
-
-//     x << "";
-//     y << "";
-
-//     x.close();
-//     y.close();
-
-//     x.open("x.txt", fstream::app);
-//     y.open("y.txt", fstream::app);
-
-//     LISTINT::iterator i;
-
-//     for (i = bytesList.begin(); i != bytesList.end(); ++i)
-//     {
-//         x << *i << ",";
-//     }
-
-//     LISTDOUBLE::iterator d;
-
-//     for (d = timesList.begin(); d != timesList.end(); ++d)
-//     {
-//         y << *d << ",";
-//     }
-//     x.close();
-//     y.close();
-
-//     return 0;
-// }
 #include <iostream>
 #include <time.h> 
 #include <fstream>
@@ -200,9 +82,8 @@ long int get_time(int ArraySize,int StepSize, bool AccessType) {
 
 	long int AccessTime=1000000000*(mt2.tv_sec - mt1.tv_sec)+(mt2.tv_nsec - mt1.tv_nsec);
 	// cout << sizeof(*a) * ArraySize << endl;
-	return AccessTime;
+	return AccessTime/ArraySize;
 }
-
 
 void write_data(std::list<int> DataSize, std::list<int> RandomTime,  std::list<int> SerialTime){
 	ofstream x, y1, y2;
@@ -265,6 +146,7 @@ int main() {
 	int StepSize = 1;
 	int NumberExp = 50;
 	int StepExp = 1000;
+
 	run_experiment(ArraySize, StepSize, NumberExp, StepExp);
 	
 	return 0;
